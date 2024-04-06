06037d821d43a8c9b8b6f8319e7c69de93099dbe
EduNunes02 <103453043+EduNunes02@users.noreply.github.com>
2024-04-06 12:54:07 -0300

    Update Shot.py

--- Shot.py
+++ Shot.py
@@ -1,20 +1,33 @@
 from OpenGL.GL import *
+import glfw
 
 class Shot:
-    def __init__(self, position_x, position_y):
-        self.position_x = position_x
-        self.position_y = position_y
-        self.move_speed = 0.05
+    def __init__(self, x):
+        self.x = x
+        self.y = -1.0
+        self.speed = 0.1
+        self.fired = False
 
-    def move(self):
-        while (self.position_y <= 1000):
-            glTranslate(self.position_x, self.position_y + 0.025)
+    def update(self):
+        if self.fired:
+            self.y += self.speed
+            
+            if self.y > 1.0:
+                self.fired = False
+                self.y = -1.0
+"""
+            while (self.position_y <= 1000):
+            glTranslate(self.position_x, self.position_y + 0.025)"""
 
+            glTranslate(self.x, self.y, 0)
 
-    def draw(self):
-        glBegin(GL_LINES)
-        glColor3f(0.15, 1, 0.15)
-        glVertex3f(self.position_x, self.position_y, 0)
-        glColor3f(0.15, 1, 0.15)
-        glVertex3f(self.position_x, self.position_y + 0.05, 0)
-        glEnd()
+    def draw(self, x):
+        self.x = x
+        if self.fired:
+            glBegin(GL_QUADS)
+            glColor3f(1.0, 0.0, 0.0)
+            glVertex2f(self.x, self.y)
+            glVertex2f(self.x + 0.01, self.y + 0.05)
+            glVertex2f(self.x - 0.01, self.y + 0.05)
+            glVertex2f(self.x, self.y)
+            glEnd()
