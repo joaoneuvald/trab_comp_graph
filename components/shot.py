eb557db8b0a10015121820b5400e9f0ac71e2a4d
Gabriel <gabrielsantostca@gmail.com>
2024-04-06 23:56:16 -0300

    feat: implemented shot class with draw and move methods

---
+++ components/shot.py
@@ -0,0 +1,22 @@
+from OpenGL.GL import *
+
+class Shot:
+    def __init__(self, position_x, position_y):
+        print(position_x, position_y)
+        self.positionX = position_x
+        self.positionY = position_y
+        self.moveSpeed = 0.001
+
+    def move(self):
+        if self.positionY < 1.0:
+            self.positionY += self.moveSpeed
+            glTranslate(self.positionX, self.positionY, 0)
+
+
+    def draw(self):
+        glBegin(GL_LINES)
+        glColor3f(0.15, 1, 0.15)
+        glVertex3f(self.positionX, self.positionY, 0)
+        glColor3f(0.15, 1, 0.15)
+        glVertex3f(self.positionX, self.positionY + 0.05, 0)
+        glEnd()
