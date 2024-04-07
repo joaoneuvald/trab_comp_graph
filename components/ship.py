cc26274f465a0d3a7a5efe5f76d924a0f7538c6a
Gabriel <gabrielsantostca@gmail.com>
2024-04-06 23:56:46 -0300

    feat: implemented ship class with draw and move methods

---
+++ components/ship.py
@@ -0,0 +1,43 @@
+from OpenGL.GL import *
+from enum import Enum
+
+class Direction(Enum):
+    RIGHT = 1
+    LEFT = 2
+
+class Ship:
+    def __init__(self):
+        self.positionX = -0.05
+        self.positionY = -0.95
+        self.moveSpeed = 0.001
+        self.center = self.positionX + 0.05
+        self.shotOutY = -0.9
+
+    def move(self, direction: Direction):
+        if direction == Direction.RIGHT and self.positionX < 0.9:
+            self.positionX += self.moveSpeed
+        elif direction == Direction.LEFT and self.positionX > -0.99:
+            self.positionX -= self.moveSpeed
+        self.center = self.positionX + 0.05
+        glTranslate(self.positionX, self.positionY, 0)
+
+
+    def draw(self):
+        glBegin(GL_LINE_LOOP)
+        glColor3f(0.15, 1, 0.15)
+        glVertex3f(self.positionX, self.positionY, 0)
+        glColor3f(0.15, 1, 0.15)
+        glVertex3f(self.positionX, self.positionY + 0.03, 0)
+        glColor3f(0.15, 1, 0.15)
+        glVertex3f(self.positionX + 0.04, self.positionY + 0.03, 0)
+        glColor3f(0.15, 1, 0.15)
+        glVertex3f(self.positionX + 0.04, self.positionY + 0.04, 0)
+        glColor3f(0.15, 1, 0.15)
+        glVertex3f(self.positionX + 0.06, self.positionY + 0.04, 0)
+        glColor3f(0.15, 1, 0.15)
+        glVertex3f(self.positionX + 0.06, self.positionY + 0.03, 0)        
+        glColor3f(0.15, 1, 0.15)
+        glVertex3f(self.positionX + 0.1, self.positionY + 0.03, 0)
+        glColor3f(0.15, 1, 0.15)
+        glVertex3f(self.positionX + 0.1, self.positionY, 0)
+        glEnd()
