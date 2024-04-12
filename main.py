2744f352589cee11aa674dab5946eea575d3ad15
Gabriel Santos <gabrielsantostca@gmail.com>
2024-04-11 22:00:00 -0300

    refactor: changed collision method

--- main.py
+++ main.py
@@ -3,30 +3,30 @@ from OpenGL.GL import *
 from components.ship import *
 from components.enemy import *
 from components.shot import Shot
-import numpy as np
 import time
 
 def check_collision():
     global shots, enemies
     for shot in shots:
         shotRemoved = False
-        for i in range(2,  0, -1):
-            print(i)
+        for i in range(2,  -1, -1):
             for enemy in enemies[i]:
                 if enemy.alive:
-                    if (shot.positionY >= enemy.positionY - 0.05 or shot.positionY <= enemy.positionY) and (shot.positionX >= enemy.positionX - 0.05 and shot.positionX <= enemy.positionX + 0.05):
+                    if (shot.positionY >= enemy.positionY - 0.05 and shot.positionY <= enemy.positionY) and (shot.positionX >= enemy.positionX - 0.05 and shot.positionX <= enemy.positionX + 0.05):
                         enemy.alive = False
                         shots.remove(shot)
                         shotRemoved = True
+                        enemies[i].remove(enemy)
                         break
-                # if abs(s hot.positionY - enemy.positionY) < 0.05 and abs(shot.positionX - enemy.positionX) < 0.2:
-                #     enemy.alive = False
-                #     shots.remove(shot)
             if shotRemoved:
                 break
 
 def checkEndGame():
     global enemies
+    if not any(enemies):
+        print("Parabéns linguiça")
+        glfw.terminate()
+        exit()
     for line in enemies:
         for enemy in line:
             if enemy.alive and enemy.positionY <= -0.85:
@@ -49,7 +49,6 @@ def main():
 
     glfw.make_context_current(window)
 
-    glMatrixMode(GL_PROJECTION)
     glLoadIdentity()
 
     for i in range(3):
@@ -61,17 +60,21 @@ def main():
     enemy = Enemy(0,0)
     shots = []
     lastShot = time.time()
+    lastDescend = time.time()
 
     enemyDirection = Direction.RIGHT
     while not glfw.window_should_close(window):
         enemyDescend = False
 
         if(enemies[0][len(enemies[0])-1].positionX >= 0.95):
-            enemyDirection = Direction.LEFT
-            enemyDescend = True
+            enemyDirection = Direction.LEFT 
         elif(enemies[0][0].positionX <= -0.95):
             enemyDirection = Direction.RIGHT
+            
+            
+        if time.time() - lastDescend >= 10:
             enemyDescend = True
+            lastDescend = time.time()
     
         glfw.poll_events()
 
@@ -85,9 +88,6 @@ def main():
                 lastShot = time.time()
         if glfw.get_key(window,glfw.KEY_ESCAPE) == glfw.PRESS:
             break;
-            
-        check_collision()
-        checkEndGame()
 
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
         
@@ -106,6 +106,9 @@ def main():
                 enemy.draw()
                 enemy.move(enemyDirection, enemyDescend)
                 glLoadIdentity()
+                
+        check_collision()
+        checkEndGame()
         
         glfw.swap_buffers(window)
 
