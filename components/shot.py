7a2da02ce76146e026cc0a0704e17a2f50320e85
Gabriel Santos <gabrielsantostca@gmail.com>
2024-04-11 21:59:40 -0300

    refactor: changed shot draw method

--- components/shot.py
+++ components/shot.py
@@ -1,22 +1,23 @@
 from OpenGL.GL import *
 
+
 class Shot:
     def __init__(self, position_x, position_y):
         print(position_x, position_y)
         self.positionX = position_x
         self.positionY = position_y
-        self.moveSpeed = 0.0001
+        self.moveSpeed = 0.01
 
     def move(self):
         if self.positionY < 1.0:
             self.positionY += self.moveSpeed
-            #glTranslate(self.positionX, self.positionY, 0)
-
+            glTranslate(self.positionX, self.positionY, 0)
 
     def draw(self):
-        glBegin(GL_LINE_LOOP)
-        glColor3f(0.15, 1, 0.15)
-        glVertex3f(self.positionX, self.positionY, 0)
-        glColor3f(0.15, 1, 0.15)
-        glVertex3f(self.positionX, self.positionY + 0.05, 0)
-        glEnd()
+            glBegin(GL_TRIANGLES)
+            glColor3f(1.0, 0.0, 0.0)
+            glVertex2f(self.positionX, self.positionY)
+            glVertex2f(self.positionX + 0.01, self.positionY + 0.05)
+            glVertex2f(self.positionX - 0.01, self.positionY + 0.05)
+            glVertex2f(self.positionX, self.positionY)
+            glEnd()
