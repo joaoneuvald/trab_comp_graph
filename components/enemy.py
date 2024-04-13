d4bcda30dc2b88227de99cb7043bbbfd96c7b5c3
joaoneuvald <joaovitorneuvald6@gmail.com>
2024-04-13 10:10:35 -0300

    feat: added enemy sprite

--- components/enemy.py
+++ components/enemy.py
@@ -1,5 +1,7 @@
 from OpenGL.GL import *
+from PIL import Image
 from components.ship import Direction
+import numpy as np
 
 class Enemy:
     def __init__(self, lineIndex, columnIndex):
@@ -7,6 +9,21 @@ class Enemy:
         self.positionY = 0.92 - lineIndex * 0.13
         self.speed = 0.001
         self.alive = True
+        
+        image_path = "assets/sprite_2.png"
+        self.texture_id = self.load_texture(image_path)
+
+    def load_texture(self, image_path):
+        img = Image.open(image_path)
+        img_data = np.array(list(img.getdata()), np.uint8)
+        
+        texture_id = glGenTextures(1)
+        glBindTexture(GL_TEXTURE_2D, texture_id)
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR)
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR)
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, img.width, img.height, 0, GL_RGBA, GL_UNSIGNED_BYTE, img_data)
+        
+        return texture_id
 
     def move(self, direction: Direction, descend: bool = False):
         if self.alive:
@@ -23,9 +40,14 @@ class Enemy:
 
     def draw(self):
         if self.alive:
-            glBegin(GL_TRIANGLES)
-            glColor3f(0.0, 1.0, 0.0)
-            glVertex2f(self.positionX, self.positionY + 0.05)
-            glVertex2f(self.positionX - 0.05, self.positionY - 0.05)
-            glVertex2f(self.positionX + 0.05, self.positionY - 0.05)
+            glEnable(GL_TEXTURE_2D)
+            glBindTexture(GL_TEXTURE_2D, self.texture_id)
+            
+            glBegin(GL_QUADS)
+            glColor3f(1.0, 1.0, 1.0)
+            glTexCoord2f(0.0, 1.0); glVertex2f(self.positionX - 0.05, self.positionY + 0.05)
+            glTexCoord2f(0.0, 0.0); glVertex2f(self.positionX - 0.05, self.positionY - 0.05)
+            glTexCoord2f(1.0, 0.0); glVertex2f(self.positionX + 0.05, self.positionY - 0.05)
+            glTexCoord2f(1.0, 1.0); glVertex2f(self.positionX + 0.05, self.positionY + 0.05)
             glEnd()
+            glDisable(GL_TEXTURE_2D)
